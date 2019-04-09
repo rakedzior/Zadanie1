caeea8f1d31cdf8276922e2ffb81307ecff95cca
Rafał Kędzior <ra.kedzior@gmail.com>
2019-04-09 19:29:13 +0100

    v4.0.2
    gotowe

--- router.js
+++ router.js
@@ -1,3 +1,4 @@
+/* deklaracja zmiennych i szablonu tych podstron */
 var Wszystko = { template: "wyszukiwarka.html" };
 var Wiadomosci = { template: "" };
 var Grafika = { template: "" };
@@ -7,6 +8,7 @@ var Wiecej = { template: "" };
 var Ustawienia = { template: "" };
 var Narzedzia = { template: "" };
 
+/* ustalenie sciezek odnosnikow i zmiennych z nimi powiazanych */
 var routes = [
   { path: '/', component: Wszystko },
   { path: '/wiadomosci', component: Wiadomosci},
@@ -18,11 +20,13 @@ var routes = [
   { path: '/narzedzia', component: Narzedzia},
 ];
 
+/* nowa instancja routera */
 var router = new VueRouter({
   routes: routes,
   mode: 'history',
 });
 
+/* przypisanie routera do miejsca w kodzie */
 var app = new Vue({
   el: '#app2',
   router: router
